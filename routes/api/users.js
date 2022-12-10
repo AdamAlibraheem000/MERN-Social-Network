9befb44c3ffc4f83a90ee52d4a402dc9045d81fd
adamalibraheem000 <adamalibraheem@hotmail.com>
2022-12-10 12:31:20 -0500

    added jwt token

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jsonwebtoken = require('jsonwebtoken');
 const {check, validationResult} = require("express-validator/check");
+const config = require('config');
+
+const User = require("../../models/User");
 
 
 router.post('/', [
@@ -8,7 +13,7 @@ router.post('/', [
     check('email', 'Email Required').isEmail(),
 ],
 
-(req,res) => {
+async (req,res) => {
     const errors = validationResult(req);
 
     // if errors due occur
@@ -16,7 +21,60 @@ router.post('/', [
         // 400. Bad Request
         return res.status(400).json({errors: errors.array()});
     }
-    res.send("User post route")
+
+    const { name, email, password} = req.body;
+
+    try{
+        // Check if user exists by email
+
+        let user = await User.findOne({email})
+
+        // If user exists
+        if(user){
+           return res.status(400).json({errors: [{message: "User already exixts"}]}); 
+        }
+
+        user = new User({
+            name,
+            email,
+            password
+        })
+
+    // Encrypt password
+
+    const salt = await bcrypt.genSalt(10);
+
+    // hash password
+    user.password = await bcrypt.hash(password, salt);
+
+    // Save user to database
+    await user.save();
+
+
+    // Return jsonwebtoken
+
+
+    const payload = {
+        user:{
+            id: user.id
+        }
+    }
+
+    jsonwebtoken.sign(payload, 
+    config.get('jwtToken'),
+    {expiresIn: 360000},
+    (error, token) => {
+        if(error) throw error;
+        res.json({token});
+    });
+
+    }catch(e){
+        console.error(e.message);
+        res.send(500).send('Server Error');
+    }
+    
+
+    
 });
 
 
